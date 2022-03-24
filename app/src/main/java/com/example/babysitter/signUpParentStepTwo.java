60c6a91ab2f4f089f6e3e277b72499fe9ce10929
Jowanna12 <jow.isaac@gmail.com>
2022-03-24 16:40:08 +0200

    sign up for parents' interface

---
+++ app/src/main/java/com/example/babysitter/signUpParentStepTwo.java
@@ -0,0 +1,14 @@
+package com.example.babysitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signUpParentStepTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_parent_step_two);
+    }
+}
