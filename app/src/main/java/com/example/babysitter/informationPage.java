b10cf55866c30f65ba48d4e151c6b7ae731c3318
Jowanna12 <jow.isaac@gmail.com>
2022-03-29 14:25:09 +0300

    opened information page and designed it

--- app/src/main/java/com/example/babysitter/informationPage.java
+++ app/src/main/java/com/example/babysitter/informationPage.java
@@ -2,7 +2,10 @@ package com.example.babysitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class informationPage extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class informationPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_page);
+    }
+
+    public void sendFeedbackViaEmail(View view) {
+
+        Intent sendEmail = new Intent(Intent.ACTION_SEND);
+        sendEmail.setData(Uri.parse("mailto:"));
+        sendEmail.setType("message/rfc822");
+        sendEmail.putExtra(Intent.EXTRA_EMAIL, new String[]{
+                "jow.isaac@gmail.com"
+        });
+        sendEmail.putExtra(Intent.EXTRA_SUBJECT, "Babysitter finder application");
+        sendEmail.putExtra(Intent.EXTRA_TEXT, "Please share us your opinion about our application!");
+        startActivity(sendEmail);
+    }
+
+    public void shareApp(View view) {
+
+        String txtShare = "Babysitter finder application";
+        String bodyOfTxt = "You can download this application for free!";
+
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
+        share.putExtra(Intent.EXTRA_TEXT , txtShare + "\n" + bodyOfTxt);
+        startActivity(Intent.createChooser(share , "Application Share"));
     }
 }
