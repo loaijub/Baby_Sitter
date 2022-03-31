02ee777175ac7a564fb946412ab346fbe3a78bad
Jowanna12 <jow.isaac@gmail.com>
2022-03-31 18:27:26 +0300

    opened splash and designed it

---
+++ app/src/main/java/com/example/babysitter/Splash.java
@@ -0,0 +1,37 @@
+package com.example.babysitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Thread thread = new Thread(){
+            public void run(){
+
+                try{
+                    sleep(4000);
+                }catch (Exception e){
+
+                }finally {
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+
+
+    }
+}
