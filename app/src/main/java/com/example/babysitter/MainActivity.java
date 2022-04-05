d093f96b4bcb482415c884decf8b7eb3dd0e857f
loai <lo2ayjub@gmail.com>
2022-04-05 15:07:20 +0300

    5/4 loai

--- app/src/main/java/com/example/babysitter/MainActivity.java
+++ app/src/main/java/com/example/babysitter/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.babysitter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
 
@@ -20,11 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment,new homePage()).commit();
 
-
     }
-
-
-
-
 
 }
