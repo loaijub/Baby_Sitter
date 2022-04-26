f3bdb7c911c577fe7ff0cf380b6d916eb74a9553
Jowanna12 <jow.isaac@gmail.com>
2022-04-26 14:47:35 +0300

    made the history for the menu

--- app/src/main/java/com/example/babysitter/Profile.java
+++ app/src/main/java/com/example/babysitter/Profile.java
@@ -6,13 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Profile extends Fragment {
 
-    @NonNull
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState){
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         return inflater.inflate(R.layout.profile, container,false);
     }
 }
