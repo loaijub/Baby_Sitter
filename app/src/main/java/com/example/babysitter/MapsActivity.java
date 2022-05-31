6e157f36f1781f572bb169ff79c8ba0b376b06a8
loai <lo2ayjub@gmail.com>
2022-05-31 14:54:50 +0300

    updated

--- app/src/main/java/com/example/babysitter/MapsActivity.java
+++ app/src/main/java/com/example/babysitter/MapsActivity.java
@@ -1,25 +1,37 @@
 package com.example.babysitter;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.babysitter.databinding.ActivityMapsBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -49,11 +61,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.homeIcon:
                         title.setText("Employees around you");
-                            for(int i=0; i<getSupportFragmentManager().getBackStackEntryCount();i++)
-                                getSupportFragmentManager().popBackStack();
+                        for (int i = 0; i < getSupportFragmentManager().getBackStackEntryCount(); i++)
+                            getSupportFragmentManager().popBackStack();
                         break;
 
                     case R.id.profileIcon:
@@ -75,8 +87,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
 
 
-
     }
+
     @Override
     public void onBackPressed() {
         getSupportFragmentManager().popBackStack();
@@ -95,15 +107,71 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        String location = "shefaram";
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        double[] cordinates = new double[2];
+        try {
+            cordinates = getLocationFromAddress(location);
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        LatLng haifa = new LatLng(cordinates[0], cordinates[1]);
 
-        LatLng haifa  = new LatLng(32.7996175,35.0517955);
+        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.profile_icon);
+        Bitmap smallMarker = Bitmap.createScaledBitmap(bitmapdraw.getBitmap(), 170, 120, false);
 
-        mMap.addMarker(new MarkerOptions().position(haifa).title("Haifa").icon(null)).showInfoWindow();
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(haifa,16));
+        mMap.addMarker(new MarkerOptions().position(haifa).title("David").icon(BitmapDescriptorFactory.fromBitmap(smallMarker))).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(haifa, 16));
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this, "requesting location", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+            return;
+        } else {
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
         // Add a marker in Sydney and move the camera
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+    }
+
+    public double[] getLocationFromAddress(String strAddress) throws IOException {
+        double[] point = new double[2];
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+
+        try {
+            address = coder.getFromLocationName(strAddress,5);
+            if (address==null) {
+                return null;
+            }
+            Address location=address.get(0);
+            point[0] = location.getLatitude();
+            point[1] = location.getLongitude();
+
+        }
+        catch(Exception e){
+            Toast.makeText(this, "Exception: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        return point;
     }
 }
